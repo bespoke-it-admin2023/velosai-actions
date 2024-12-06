76aba4a8e8ad6cb990f353f467108d056451025e
bespoke-itadmin-2023 <jomer.bespoke@gmail.com>
2024-12-06 14:55:56 +0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
 
     try {
         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-            model: 'gpt-3.5-turbo', // or use 'gpt-4' if you have access
+            model: 'gpt-4o-mini', // or use 'gpt-4' if you have access
             messages: [{ role: 'user', content: userInput }],
         }, {
             headers: {
